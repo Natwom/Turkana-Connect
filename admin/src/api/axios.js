c889023a5433626aaa2ffba53ff6bdba5e4d8961
Natwom Daniel <natwomdaniel@gmail.com>
2026-06-27 05:26:43 -0400

    apiaro

--- admin/src/api/axios.js
+++ admin/src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://turkana-connect-api.onrender.com/api/v1',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+  baseURL: 'https://turkana-connect-api.onrender.com'
 })
 
 api.interceptors.request.use((config) => {
