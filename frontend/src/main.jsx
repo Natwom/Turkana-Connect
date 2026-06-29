a64c837e528f5b9c40144ef3849b3b1b0324a94d
Natwom Daniel <natwomdaniel@gmail.com>
2026-06-29 08:55:08 -0400

    switch to HashRouter for SPA refresh fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { PlayerProvider } from './context/PlayerContext'
 import App from './App'
-import './index.css'  // <-- THIS LINE MUST EXIST
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <PlayerProvider>
           <App />
         </PlayerProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
