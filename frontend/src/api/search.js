7e5d7fd39786d39cb3b7bb805cb5d90493c9bf34
Natwom Daniel <natwomdaniel@gmail.com>
2026-06-29 08:34:52 -0400

    fix search API path - add /api/v1 prefix

--- frontend/src/api/search.js
+++ frontend/src/api/search.js
@@ -1,6 +1,6 @@
 import api from './axios'
 
 export const search = (query) => 
-  api.get('/search', { params: { q: query } })
+  api.get('/api/v1/search', { params: { q: query } })
 
 export default { search }
